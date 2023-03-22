66582eb60c69b532bb5a6f4db000bfad95076c52
Nancy Ramírez <51838679+Nancy-Ramirez@users.noreply.github.com>
2023-03-22 09:05:45 -0600

    ss

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-import Router from './Components/Router/Router';
+import Router from "./Components/Router/Router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router/>
+    <Router />
   </React.StrictMode>
 );
 

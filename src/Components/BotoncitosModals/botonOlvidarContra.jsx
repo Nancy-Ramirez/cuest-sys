248bb38c39d57301da23ca6a4500aad5defb0023
Nancy Ramírez <51838679+Nancy-Ramirez@users.noreply.github.com>
2023-03-30 13:59:38 -0600

    Update botonOlvidarContra.jsx

--- src/Components/BotoncitosModals/botonOlvidarContra.jsx
+++ src/Components/BotoncitosModals/botonOlvidarContra.jsx
@@ -14,7 +14,7 @@ export const BtnOlvidarContra = () => {
         id="defaultModal"
         tabindex="-1"
         aria-hidden="true"
-        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full"
+        class="fixed top-0 left-0 right-0 z-10 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full"
       >
         <div class="relative w-full h-full max-w-2xl md:h-auto">
           <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
